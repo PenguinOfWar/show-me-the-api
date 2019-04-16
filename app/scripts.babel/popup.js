223d00b9643fcefb08f123b17ded4fe4584567b3
Darryl Walker <darryl.walker@sony.com>
2019-04-16 14:23:44 +0100

    Adding new feature for showing both localised and master API links

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -11,25 +11,16 @@ chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true }, function (tabs)
   var title = tabs[0].title;
   var isSony = url.includes('pro.sony');
 
-  console.log(tabs[0]);
-
   var api = isSony ? getLocation(url) : {};
 
   /* parse the pathname */
 
-  console.log(api.pathname);
-
   var chunks = api.pathname.replace(/^\/|\/$/g, '').split('/');
+  var chunksOriginal = api.pathname.replace(/^\/|\/$/g, '').split('/');
 
-  console.log(chunks);
-
-  console.log('Running locale test');
-
-  console.log(chunks[0]);
+  var originalLocale = chunksOriginal.shift();
 
   var localeTest = /^[a-zA-Z]{2}[-_][a-zA-Z]{2}$/.test(chunks[0]);
-
-  console.log(localeTest);
 
   var locale = 'xx_XX';
 
@@ -42,11 +33,11 @@ chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true }, function (tabs)
   chunks.unshift('api/dynamicmodules');
   chunks.unshift(locale);
 
-  console.log(chunks);
+  chunksOriginal.unshift('api/dynamicmodules');
+  chunksOriginal.unshift(originalLocale);
 
   var apiPath = 'https://' + api.hostname + '/' + chunks.join('/');
-
-  console.log(apiPath);
+  var apiPathWithLocale = 'https://' + api.hostname + '/' + chunksOriginal.join('/');
 
   var message = isSony ? 'Current pro.sony page:' : 'This is not a pro.sony page!';
 
@@ -54,10 +45,19 @@ chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true }, function (tabs)
   var hiddenElem = document.getElementById('hidden');
   var linkElem = document.getElementById('link');
   var titleElem = document.getElementById('title');
+  var originalLocaleElem = document.getElementById('originalLocale');
+
+  var linkElemLocalised = document.getElementById('linkLocalised');
+  var titleElemLocalised = document.getElementById('titleLocalised');
 
   messageElem.textContent = message;
   linkElem.href = apiPath;
   titleElem.textContent = title;
+
+  linkElemLocalised.href = apiPathWithLocale;
+  titleElemLocalised.textContent = title;
+
+  originalLocaleElem.textContent = originalLocale;
 
   /* show or hide the button and the current link */
 
